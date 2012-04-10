abd5f8dd365f8b126cf7849733f4b22963fdea2e
Erik Johnson <ej@ucar.edu>
2012-04-10 11:46:02 -0600

    adding JJA's GOES EAST WMS layers; adding IOWA GOES EAST VIS WMS for comparison;

--- openlayers/iowa-mesonet/js/map.coffee
+++ openlayers/iowa-mesonet/js/map.coffee
@@ -41,36 +41,55 @@ load_map = (div_name)->
   map.setCenter ames.transform( new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()), 5
 
   # east_ir_4km
-  GOESEASTIR = 
+  GOESEASTIR =
     layer:      'east_ir_4km'
     url:        'http://mesonet.agron.iastate.edu/cgi-bin/wms/goes/east_ir.cgi?'
     visibility: true
 
-  GOESWESTIR = 
+  GOESWESTIR =
     layer: 'west_ir_4km'
     url:   'http://mesonet.agron.iastate.edu/cgi-bin/wms/goes/west_ir.cgi?'
 
-  GOESCONUSIR = 
+  GOESEASTVIS =
+    layer:      'east_vis_1km'
+    url:        'http://mesonet.agron.iastate.edu/cgi-bin/wms/goes/east_vis.cgi?'
+    visibility: true
+
+  GOESCONUSIR =
     layer: 'goes_conus_ir'
     url:   'http://mesonet.agron.iastate.edu/cgi-bin/wms/goes/conus_ir.cgi?'
 
-  NEXRADBASEREFLECT=
+  NEXRADBASEREFLECT =
     layer:      "nexrad_base_reflect"
     url:        "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0q.cgi?"
     visibility: true
 
-  NEXRADN0Q=
+  NEXRADN0Q =
     layer: "nexrad-n0q"
     url:   "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0q.cgi?"
     visibility: true
 
-  iowa_cgis = [ GOESEASTIR, GOESWESTIR, NEXRADBASEREFLECT, NEXRADN0Q ]
+  goes_east_ch1_latest =
+    url: "http://mapserver.eol.ucar.edu/jja-ce2-mapserv?"
+    layer: 'goes_east_ch1_latest',
+    visibility: true
+
+  goes_east_ch3_latest =
+    url: "http://mapserver.eol.ucar.edu/jja-ce2-mapserv?"
+    layer: 'goes_east_ch3_latest',
+
+  goes_east_ch4_latest =
+    url: "http://mapserver.eol.ucar.edu/jja-ce2-mapserv?"
+    layer: 'goes_east_ch4_latest',
+
+  iowa_cgis = [ GOESEASTIR, GOESEASTVIS, GOESWESTIR, NEXRADBASEREFLECT, NEXRADN0Q, goes_east_ch3_latest ]
+  eol_cgis  = [ goes_east_ch1_latest, goes_east_ch3_latest, goes_east_ch4_latest ]
 
   # iowa_cgis=[]
 
   for iowa_cgi in iowa_cgis
     # iowa_wms_layer = new OpenLayers.Layer.WMS "Nexrad", "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r-t.cgi?",
-    iowa_wms_layer = new OpenLayers.Layer.WMS iowa_cgi['layer'], iowa_cgi['url'],
+    iowa_wms_layer = new OpenLayers.Layer.WMS "iowa " + iowa_cgi['layer'], iowa_cgi['url'],
         layers:      iowa_cgi['layer']
         transparent: true
         format:      "image/png",
@@ -79,6 +98,18 @@ load_map = (div_name)->
     iowa_wms_layer.setOpacity .6
     iowa_wms_layer.setVisibility (iowa_cgi['visibility']? and iowa_cgi['visibility'])
     map.addLayer iowa_wms_layer
+
+  for eol_cgi in eol_cgis
+    # iowa_wms_layer = new OpenLayers.Layer.WMS "Nexrad", "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r-t.cgi?",
+    eol_wms_layer = new OpenLayers.Layer.WMS "EOL " + eol_cgi['layer'], eol_cgi['url'],
+        layers:      eol_cgi['layer']
+        transparent: true
+        format:      "image/png",
+        { transitionEffect: 'resize' }
+        # time: "2005-08-29T13:00:00Z"
+    eol_wms_layer.setOpacity .6
+    eol_wms_layer.setVisibility (eol_cgi['visibility']? and eol_cgi['visibility'])
+    map.addLayer eol_wms_layer
 
   keyboardControl = new OpenLayers.Control.KeyboardDefaults
   layerSwitcher   = new OpenLayers.Control.LayerSwitcher({'ascending':false})
--- openlayers/iowa-mesonet/js/map.js
+++ openlayers/iowa-mesonet/js/map.js
@@ -2,7 +2,7 @@
   var load_map;
 
   load_map = function(div_name) {
-    var GOESCONUSIR, GOESEASTIR, GOESWESTIR, NEXRADBASEREFLECT, NEXRADN0Q, ames, iowa_cgi, iowa_cgis, iowa_wms_layer, keyboardControl, layerSwitcher, map, navControl, osm, panZoomBar, _i, _len;
+    var GOESCONUSIR, GOESEASTIR, GOESEASTVIS, GOESWESTIR, NEXRADBASEREFLECT, NEXRADN0Q, ames, eol_cgi, eol_cgis, eol_wms_layer, goes_east_ch1_latest, goes_east_ch3_latest, goes_east_ch4_latest, iowa_cgi, iowa_cgis, iowa_wms_layer, keyboardControl, layerSwitcher, map, navControl, osm, panZoomBar, _i, _j, _len, _len2;
     OpenLayers.IMAGE_RELOAD_ATTEMPTS = 4;
     OpenLayers.Util.onImageLoadErrorColor = "transparent";
     map = new OpenLayers.Map({
@@ -31,6 +31,11 @@
       layer: 'west_ir_4km',
       url: 'http://mesonet.agron.iastate.edu/cgi-bin/wms/goes/west_ir.cgi?'
     };
+    GOESEASTVIS = {
+      layer: 'east_vis_1km',
+      url: 'http://mesonet.agron.iastate.edu/cgi-bin/wms/goes/east_vis.cgi?',
+      visibility: true
+    };
     GOESCONUSIR = {
       layer: 'goes_conus_ir',
       url: 'http://mesonet.agron.iastate.edu/cgi-bin/wms/goes/conus_ir.cgi?'
@@ -45,10 +50,24 @@
       url: "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0q.cgi?",
       visibility: true
     };
-    iowa_cgis = [GOESEASTIR, GOESWESTIR, NEXRADBASEREFLECT, NEXRADN0Q];
+    goes_east_ch1_latest = {
+      url: "http://mapserver.eol.ucar.edu/jja-ce2-mapserv?",
+      layer: 'goes_east_ch1_latest',
+      visibility: true
+    };
+    goes_east_ch3_latest = {
+      url: "http://mapserver.eol.ucar.edu/jja-ce2-mapserv?",
+      layer: 'goes_east_ch3_latest'
+    };
+    goes_east_ch4_latest = {
+      url: "http://mapserver.eol.ucar.edu/jja-ce2-mapserv?",
+      layer: 'goes_east_ch4_latest'
+    };
+    iowa_cgis = [GOESEASTIR, GOESEASTVIS, GOESWESTIR, NEXRADBASEREFLECT, NEXRADN0Q, goes_east_ch3_latest];
+    eol_cgis = [goes_east_ch1_latest, goes_east_ch3_latest, goes_east_ch4_latest];
     for (_i = 0, _len = iowa_cgis.length; _i < _len; _i++) {
       iowa_cgi = iowa_cgis[_i];
-      iowa_wms_layer = new OpenLayers.Layer.WMS(iowa_cgi['layer'], iowa_cgi['url'], {
+      iowa_wms_layer = new OpenLayers.Layer.WMS("iowa " + iowa_cgi['layer'], iowa_cgi['url'], {
         layers: iowa_cgi['layer'],
         transparent: true,
         format: "image/png"
@@ -58,6 +77,19 @@
       iowa_wms_layer.setOpacity(.6);
       iowa_wms_layer.setVisibility((iowa_cgi['visibility'] != null) && iowa_cgi['visibility']);
       map.addLayer(iowa_wms_layer);
+    }
+    for (_j = 0, _len2 = eol_cgis.length; _j < _len2; _j++) {
+      eol_cgi = eol_cgis[_j];
+      eol_wms_layer = new OpenLayers.Layer.WMS("EOL " + eol_cgi['layer'], eol_cgi['url'], {
+        layers: eol_cgi['layer'],
+        transparent: true,
+        format: "image/png"
+      }, {
+        transitionEffect: 'resize'
+      });
+      eol_wms_layer.setOpacity(.6);
+      eol_wms_layer.setVisibility((eol_cgi['visibility'] != null) && eol_cgi['visibility']);
+      map.addLayer(eol_wms_layer);
     }
     keyboardControl = new OpenLayers.Control.KeyboardDefaults;
     layerSwitcher = new OpenLayers.Control.LayerSwitcher({
